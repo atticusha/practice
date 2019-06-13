976e0290570d58d7f8378d912f65dd376d8934a6
atticusha <37557159+atticusha@users.noreply.github.com>
2019-06-13 14:45:19 -0600

    Update and rename java dog.js to javadog.js

--- javadog.js
+++ javadog.js
@@ -1,11 +1,4 @@
 Math.floor(Math.random() * 10)
-2
 let dogs = [‘bertrand’, ‘woofers’, ‘mikey’]
-undefined
 dogs.length
-3
-Math.floor(Math.random() * dogs.length)
-2
-Math.floor(Math.random() * dogs.length)
-0
 Math.floor(Math.random() * dogs.length)
